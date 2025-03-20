faef04ce4df57a5f17adf0584cb1cef4ee3844cb
LisaJ5472 <lisaj5472@gmail.com>
2025-03-20 12:55:05 -0600

    moved styles to public folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "/src/assets/styles.css";
+// import "/src/assets/styles.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
