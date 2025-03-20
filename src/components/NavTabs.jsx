79aa3adc21e2d33a4f44c9aba6703e48b9ae584a
LisaJ5472 <lisaj5472@gmail.com>
2025-03-19 22:05:33 -0600

    added name to navbar

--- src/components/NavTabs.jsx
+++ src/components/NavTabs.jsx
@@ -1,18 +1,24 @@
-import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import React from 'react';
+import logo from '../assets/logo.png'; 
 
-export default function NavTabs() {
+const Navbar = () => {
   return (
     <nav className="navbar">
-      <a href="/">
-        <img src={logo} alt="LAJ Logo" className="logo" />
-      </a>  
+      <div className="logo-container">
+        <a href="/">
+          <img src={logo} alt="LAJ Logo" className="logo" />
+        </a>
+        <span className="name">Lisa Jorgensen</span> 
+      </div>
+
       <ul className="nav-links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/portfolio">Portfolio</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
     </nav>
   );
-}
+};
+
+export default Navbar;
