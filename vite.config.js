b70c62779d40ed395987b1c6d6bb46d75f0551df
LisaJ5472 <lisaj5472@gmail.com>
2025-03-20 11:39:49 -0600

    added build rollup options for netlify

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,14 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['/src/assets/styles.css'],
-    },
+      external: ['/src/assets/images/headShot.jpg'],
+      external: ['/src/assets/logo.png'],
+      external: ['/src/assets/docs/lj-full-resume.pdf'],
+      external: ['src/assets/images/blog.png'],
+      external: ['src/assets/images/CSSsnipperCheatsheet.png'],
+      external: ['src/assets/images/digest.png'], 
+      external: ['src/assets/images/firstPortfolio.png'],
+      external: ['src/assets/docs/lj-full-resume.pdf'],
+       },
 }
 });
