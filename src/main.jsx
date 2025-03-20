e43f37ca5c916c663052f84c03f4d9d9fdf7e2af
LisaJ5472 <lisaj5472@gmail.com>
2025-03-20 11:03:25 -0600

    used absolute path for styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './assets/styles.css';
+import "/src/assets/styles.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
