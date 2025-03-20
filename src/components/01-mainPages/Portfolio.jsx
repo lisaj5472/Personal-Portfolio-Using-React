8ce2bbbcf8dcc22c0db86f6a166b0b77d91c7f64
LisaJ5472 <lisaj5472@gmail.com>
2025-03-20 00:43:44 -0600

    updated portfolio card grid

--- src/components/01-mainPages/Portfolio.jsx
+++ src/components/01-mainPages/Portfolio.jsx
@@ -17,31 +17,43 @@ export default function Portfolio() {
           <li>PostgreSQL</li>
         </ul>
 
-        <p>Click on the links below to view some of the projects I have worked on.</p>
+        <p>Click on the links below to view some of the projects I have worked on. Please note: this website was built using original code, however, some of the code and design elements for the projects below were provided by the University of Denver Coding Boot Camp educational team as part of the course.</p>
       </div>
 
-      <div className="page container portfolio-grid">
-      <PortfolioCard
+      <div className="portfolio-grid">
+      <PortfolioCard className="portfolio-card"
           title="Digest"
           description="This group project is a personalized nutrition app that generates meal plans based on user preferences and dietary restrictions. It features a user-friendly interface, recipe search functionality, and a meal plan generator. This project combined the skills of a team of developers to create a full-stack application using HTML, CSS, Bootstrap, and Javascript."
           image="/assets/images/digest.png"
           link="https://justinvittitoe.github.io/Digest/"
         />
-        <PortfolioCard
+        <PortfolioCard className="portfolio-card"
           title="Vehicle Management CLI Application"
           description="This is a command-line interface (CLI) application for managing vehicles, including Cars, Trucks, and Motorbikes. Written with typescript, the program allows users to interact with a collection of vehicles, generate VIN numbers, and perform various operations through an interactive prompt."
           video="https://drive.google.com/file/d/1-5Ss3KMQI3DIpRONd7DZIjdexxz1ZAu7/view?usp=sharing"
         />
-        <PortfolioCard
+        <PortfolioCard className="portfolio-card"
           title="CSS Snippet Cheatsheet"
           description="This project is a CSS Snippet Cheat Sheet developed as part of the University of Denver Coding Boot Camp. It serves as a handy reference for commonly used CSS snippets and techniques, facilitating quick access to essential code patterns for web development."
           image="/assets/images/CSSsnippetCheatsheet.png"
           link="https://lisaj5472.github.io/CSS-Snippet-Cheatsheet/"
         />
-        <PortfolioCard
+        <PortfolioCard className="portfolio-card"
           title="SQL Employee Tracker CLI"
           description="This command-line interface (CLI) application manages employees, departments, and roles within a company SQL database. It provides an interactive menu to view, add, and update employee records efficiently. The program uses Node.js, Inquirer, and postgreSQL to interact with the database."
           video="https://drive.google.com/file/d/1-5iy4k8lY0a6JyE28rC_VMdG1KRE2woY/view?usp=sharing"
+        />
+        <PortfolioCard className="portfolio-card"
+          title="First Portfolio - CSS Challenge"
+          description="This was my first portfolio project attempt. It was built using very basic HTML and CSS styling."
+          image="/assets/images/firstPortfolio.png"
+          link="https://lisaj5472.github.io/firstPortfolio-CSSchallenge/"
+        />
+        <PortfolioCard className="portfolio-card"
+          title="Personal Blog - web APIs Challenge"
+          description="This project is a two-page personal blog website developed as part of the University of Denver Coding Boot Camp. It enables users to input and view blog posts, featuring a content form, dynamic rendering of blog entries, and a light/dark mode toggle. The website is built using HTML, CSS, and Javascript."
+          image="/assets/images/blog.png"
+          link="https://lisaj5472.github.io/personalBlog-webAPIsChallenge/"
         />
         </div>
 </div>
